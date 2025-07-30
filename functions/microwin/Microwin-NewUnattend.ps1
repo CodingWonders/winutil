9f4c22a3a098b73a674a996493b31de9b5d40bc0
Real-MullaC <callumjanes2007new+github@gmail.com>
2025-07-30 21:07:50 +0100

    Update Microwin-NewUnattend.ps1

--- functions/microwin/Microwin-NewUnattend.ps1
+++ functions/microwin/Microwin-NewUnattend.ps1
@@ -76,7 +76,6 @@ function Microwin-NewUnattend {
         </settings>
     </unattend>
 '@
-
     $specPass = @'
 <settings pass="specialize">
         <component name="Microsoft-Windows-SQMApi" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
